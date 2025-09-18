c87700220c0d7d8977b571bb22b18c694de4f51a
Franco <rojasfranco204@gmail.com>
2025-09-18 18:05:02 -0300

    Fix nombres de imágenes en minúsculas para Vercel

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import project1 from "../assets/Project1.png";
+import project1 from "../assets/project1.png";
 import project2 from "../assets/project2.png";
 import project3 from "../assets/project3.png";
 
